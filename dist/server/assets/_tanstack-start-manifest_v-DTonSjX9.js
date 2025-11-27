68f1fee287ddac309d35bad8af99c3747f659354
Francis Amidu <francisamidu124@gmail.com>
2025-11-27 16:37:16 +0200

    Fixed link issue

--- dist/server/assets/_tanstack-start-manifest_v-DTonSjX9.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/francis/Documents/Dev/Chitsulo/src/app/__root.tsx", "children": ["/"], "preloads": ["/assets/main-ckgA2uAv.js"], "assets": [{ "tag": "link", "attrs": { "rel": "stylesheet", "href": "/assets/main-CbvegwjA.css", "type": "text/css" } }] } }, "clientEntry": "/assets/main-ckgA2uAv.js" });
-export {
-  tsrStartManifest
-};
