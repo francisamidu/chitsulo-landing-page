7928b1a2cfa5925afcabee86da3dc2afbd9b4ae0
Francis Amidu <francisamidu124@gmail.com>
2025-11-27 16:52:52 +0200

    Fixed 404 routing issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+import path from "path";
 // vite.config.ts
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 5175,
   },
